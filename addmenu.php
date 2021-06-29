7fb2bce3fa44205ff044193b073a9d8966bd58f2
eryry <63179394+eryry@users.noreply.github.com>
2021-06-30 00:00:42 +0900

    jqueryCDNURL更新

--- addmenu.php
+++ addmenu.php
@@ -13,11 +13,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>BAR MENU</title>
   <link href="style.css" rel="stylesheet">
+
   <script
   src="https://code.jquery.com/jquery-3.6.0.min.js"
   integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
   crossorigin="anonymous"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
 </head>
 <body>
   <main>
@@ -29,9 +30,9 @@
 
         <p>メニュー登録</p>
         <form action="" method="post">
-          <p>カテゴリー：<input type="number" name="m_cat" id=""></p>
-          <p>メニュー名：<input type="text" name="m_name" id=""></p>
-          <p>値段：<input type="number" name="m_price" id="">円</p>
+          <p>カテゴリー：<input type="number" name="m_cat" id="m_cat"></p>
+          <p>メニュー名：<input type="text" name="m_name" id="m_name"></p>
+          <p>値段：<input type="number" name="m_price" id="m_price">円</p>
           <input  id="add_menu" type="submit" value="登録する">
           
         </form>
@@ -46,7 +47,6 @@
     </div>
 
    
-
   </main>
 	<script src="script.js"></script>
   
--- script.js
+++ script.js
@@ -26,26 +26,25 @@ $('#add_menu').on('click',function(){
 
   if(confirmResult) {
       $.ajax({
-          type: "POST", // 送信方法
-          url: "exec_addmenu.php", // 送信先ファイル名
-          datatype: "json", // 受け取りデータの種類
-          // 送信データ
+          type: "POST", // HTTP通信の種類を指定
+          url: "exec_addmenu.php", // リクエスト先のURL
+          datatype: "json", // サーバに送信する値
           data: {
-              "m_cat" : $('#m_cat').val(), // #nameと#priceのvalueをセット
+              "m_cat" : $('#m_cat').val(), // 送信データ
               "m_name" : $('#m_name').val(),
               "m_price" : $('#m_price').val()
           },
           // 通信が成功した時
           success: function(data) {
-              $('#add_result').html("<p>" + data[0].m_name + "が" + data[0].m_price + "円のデータを登録しました。</p>");
+              $('#add_result').html("<p>" + data[0].m_name + "：" + data[0].m_price + "円のメニューを登録しました。</p>");
 
               console.log("通信成功");
               console.log(data);
 
               // 一覧に追加したレコードを追記
-              $.each(data, function(key, value){
-                  $('#all_show_result').append("<tr><td>" + value.id + "</td><td>" + value.name + "</td><td>" + value.price + "</td></tr>");
-              });
+              // $.each(data, function(key, value){
+              //     $('#all_show_result').append("<tr><td>" + value.id + "</td><td>" + value.name + "</td><td>" + value.price + "</td></tr>");
+              // });
           },
 
           // 通信が失敗した時
